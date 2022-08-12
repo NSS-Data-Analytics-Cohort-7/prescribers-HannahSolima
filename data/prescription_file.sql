eb7e780d4985c44c13ba087d09b8f7b12c803515
Hannah Solima <hannahsolima@gmail.com>
2022-08-11 22:48:12 -0500

    made corrections and on 5.c

--- data/prescription_file.sql
+++ data/prescription_file.sql
@@ -23,74 +23,78 @@ SELECT *
 FROM zip_fips;
 
 ---Question 1.a
-SELECT p1.npi, total_claim_count
+SELECT p1.npi, SUM(total_claim_count) AS total_claim_count
 FROM prescriber AS p1
-LEFT JOIN prescription AS p2
-ON p1.npi = p2.npi
-WHERE total_claim_count IS NOT NULL
+JOIN prescription AS p2
+USING (npi)
+GROUP BY p1.npi
 ORDER BY total_claim_count DESC;
----ANSWER: 1912011792 (npi), 4538 (total_claim_count)
+---ANSWER: 1881634483 (npi), 99707 (total_claim_count)
 
 ---QUESTION 1.b
-SELECT p1.npi, total_claim_count, nppes_provider_first_name, nppes_provider_last_org_name, specialty_description
+SELECT p1.npi, 
+nppes_provider_first_name, 
+nppes_provider_last_org_name, 
+specialty_description,
+SUM(total_claim_count) AS total_claim_count
 FROM prescriber AS p1
 LEFT JOIN prescription AS p2
 ON p1.npi = p2.npi
 WHERE total_claim_count IS NOT NULL 
+GROUP BY p1.npi, 
+nppes_provider_first_name, 
+nppes_provider_last_org_name, 
+specialty_description
 ORDER BY total_claim_count DESC;
----ANSWER: DAVID COFFEY, Family Practice (specialty description) 
+---ANSWER: Bruce Pendley, Family Practice (specialty description) 
 
 ---QUESTION 2.a
 SELECT p1.specialty_description, SUM(total_claim_count) AS TotalClaimCount
 FROM prescriber AS p1
-LEFT JOIN prescription AS p2 
+JOIN prescription AS p2 
 ON p1.npi = p2.npi
-WHERE total_claim_count IS NOT NULL 
 GROUP BY p1.specialty_description
 ORDER BY SUM(total_claim_count) DESC;
 ---ANSWER: Family Practice, 9752347 (total_claim_count)
 
 ---QUESTION 2.b
-SELECT p1.specialty_description, COUNT(opioid_drug_flag) AS opioid_count
+SELECT p1.specialty_description, SUM(total_claim_count) AS claim_count
 FROM prescriber AS p1
-LEFT JOIN prescription AS p2 
+JOIN prescription AS p2 
 USING (npi)
-LEFT JOIN drug AS d
+JOIN drug AS d
 USING (drug_name)
-WHERE total_claim_count IS NOT NULL
-AND opioid_drug_flag = 'Y'
+WHERE opioid_drug_flag = 'Y'
 GROUP BY p1.specialty_description
-ORDER BY opioid_count DESC;
----ANSWER: Nurse Practitioner, 9551 (opioid_count)
+ORDER BY claim_count DESC;
+---ANSWER: Nurse Practitioner, 900845 (claim_count)
 
 ---QUESTION 2.c (challenge question)
-SELECT p1.specialty_description, SUM(total_claim_count) AS TotalClaimCount
+SELECT p1.specialty_description
 FROM prescriber AS p1
 LEFT JOIN prescription AS p2 
 ON p1.npi = p2.npi
-WHERE total_claim_count IS NULL
+WHERE p2.npi IS NULL
 GROUP BY p1.specialty_description;
 ---ANSWER: 92 specialities? 
 
 ---QUESTION 3.a 
-SELECT generic_name, ROUND(MAX(total_drug_cost),0) AS total_drug_cost
+SELECT generic_name, ROUND(MAX(total_drug_cost),2) AS total_drug_cost
 FROM drug AS d
-LEFT JOIN prescription AS p
+JOIN prescription AS p
 USING (drug_name)
-WHERE total_drug_cost IS NOT NULL
 GROUP BY generic_name
 ORDER BY total_drug_cost DESC;
----ANSWER: PIRENIDONE, 2829174
+---ANSWER: PIRENIDONE, 2829174.30 (total_drug_cost)
 
 ---QUESTION 3.b 
-SELECT generic_name, ROUND(MAX(total_drug_cost)/365,2) AS daily_cost
+SELECT generic_name, ROUND(MAX(total_drug_cost/p.total_day_supply),2) AS daily_cost
 FROM drug AS d
-LEFT JOIN prescription AS p
+JOIN prescription AS p
 USING (drug_name)
-WHERE total_drug_cost IS NOT NULL
 GROUP BY generic_name
 ORDER BY daily_cost DESC;
----ANSWER: PIRFENIDONE, 7751.16 (daily cost)
+---ANSWER: "IMMUN GLOB G(IGG)/GLY/IGA OV50", 7141.11 (daily cost)
 
 ---QUESTION 4.a
 SELECT drug_name, 
@@ -100,4 +104,30 @@ SELECT drug_name,
 FROM drug AS d;
 
 ---QUESTION 4.b 
+SELECT (CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
+ WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic' 
+ ELSE 'NEITHER' END) AS drug_type, SUM(CAST(total_drug_cost AS money)) AS drug_cost
+FROM drug AS d
+JOIN prescription AS p
+USING (drug_name)
+GROUP BY drug_type
+ORDER BY drug_cost DESC;
+---ANSWER: Opioid, $105M
+
+---QUESTION 5.a
+SELECT COUNT(*)
+FROM cbsa
+WHERE cbsaname LIKE '%TN%';
+---ANSWER: 56
+
+---QUESTION 5.b
+SELECT cbsaname, SUM(population) AS pop
+FROM cbsa
+JOIN population
+USING (fipscounty)
+GROUP BY cbsaname
+ORDER BY pop DESC;
+---ANSWER: "Nashville-Davidson--Murfreesboro--Franklin, TN", 1830410 (largest)
+---Morristown,TN, 116352 (smallest)
+
 
